c34f9bf3d9f6884817bbd9e1bbba6a22408ef4e8
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-03-22 17:23:39 +0200

    feat: add nav skeloton

--- src/components/sections/homepage/NavSkeleton.jsx
+++ src/components/sections/homepage/NavSkeleton.jsx
@@ -1,7 +1,7 @@
 import Skeleton from "react-loading-skeleton";
 
-export default function NavSkeleton() {
-  return Array(5)
+export default function NavSkeleton({ count }) {
+  return Array(count)
     .fill()
     .map((_, index) => <Skeleton key={`tab-skeleton-${index}`} width={100} />);
 }
